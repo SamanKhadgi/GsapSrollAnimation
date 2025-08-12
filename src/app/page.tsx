e11bdcfaf02b7567e902c727ea840fb2276a3e7c
Saman <samankhadgi@gmail.com>
2025-08-12 14:15:32 +0545

    layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -9,7 +8,6 @@ export default function Home() {
   ScrollTrigger.refresh();
   return (
     <div className="font-sans min-h-screen">
-      <Header />
       <Hero />
       <div className="h-screen"></div>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
